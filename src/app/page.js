10708230799a2669537d0826515ec31eab60e334
Mahesh Darkunde <maheshdarkunde2@gmail.com>
2026-01-29 16:08:47 +0530

    hero image fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
+
 export default function Page() {
   return (
-    <div className="container mx-auto p-4">
-      <section className="text-center my-8 bg-[url('/images/home/home-hero-gradient.svg')] bg-cover bg-center py-24 height-96 flex items-center justify-center  border border-amber-200 rounded-lg ">
-        <h1 className="text-4xl text-white font-bold">Welcome</h1>
+    <>
+      <section>
+        <div className=" mx-auto p-4 bg-cover bg-center"
+          style={{ backgroundImage: "url('/images/home/home-hero-gradient.svg')" }}>
+          <div className="container mx-auto  ">
+            <section className="h-96 p-8 "  >
+              <h1 className="text-4xl text-white font-bold">Welcome to Block Chain</h1>
+            </section>
+
+          </div>
+        </div>
       </section>
-    </div>
+      <section>
+        <div className=" mx-auto p-4 bg-cover bg-center"
+          style={{ backgroundImage: "url('/images/home/home-hero-gradient.svg')" }}>
+          <div className="container mx-auto  ">
+            <section className="h-96 p-8 "  >
+              <h1 className="text-4xl text-white font-bold">Welcome to Block Chain</h1>
+            </section>
+
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
